26f352a47d4f8ce68eae0d9d23690610628e3d1a
EbsaTeklu <ebsa16teklu@gmail.com>
2022-08-13 11:52:09 +0300

    employee routes - added GET PATCH DELETE

--- src/routes/employee.js
+++ src/routes/employee.js
@@ -1,7 +1,42 @@
 const express=require("express")
 const mongoose=require("mongoose")
+const employee = require("../model/employee")
 const Employee=require("../model/employee")
 const router=express.Router()
+router.get('/',(req,res)=>{
+    Employee.find()
+    .exec()
+    .then(employees=>{
+        return res.status(200).json({
+            count:employees.length,
+            employees:employees
+        })
+    })
+    .catch(err=>{
+        return res.status(500).json({
+            message:"couldn't execute find",
+            error:err
+        })
+    })
+})
+router.get('/:id',(req,res)=>{
+    const id=req.params.id
+    Employee.findById(id).exec()
+    .then(emp=>{
+        if (!emp) {
+            return res.status(404).json({
+                message:"employee with the specified id not found"
+            })
+        }
+        return res.status(200).json(emp)
+    })
+    .catch(err=>{
+        return res.status(500).json({
+            message:"couldn't execute find",
+            error:err
+        })
+    })
+})
 router.post('/',(req,res)=>{
     const employee=new Employee({
         _id:mongoose.Types.ObjectId(),
@@ -15,7 +50,7 @@ router.post('/',(req,res)=>{
     })
     employee.save()
     .then(emp=>{
-        return res.status(200).json({
+        return res.status(201).json({
             message:"created",
             employee:emp
         })
@@ -25,6 +60,49 @@ router.post('/',(req,res)=>{
             message:"not created",
             error:err
         })    
+    })
+})
+router.patch("/:id",(req,res)=>{
+    const id=req.params.id
+    Employee.findByIdAndUpdate(id,{
+        name:{
+            firstname:req.body.name.firstname,
+            lastname:req.body.name.lastname
+        },
+        phone:req.body.phone,
+        salary:req.body.salary,
+        department:req.body.department
+    }).exec()
+    .then(employee=>{
+        if (!employee) {
+            return res.status(404).json({
+                message:"employee with the specified id not found"
+            })
+        }
+        return res.status(200).json({
+            message:"Employee Updated!",
+            employee:employee
+        })
+    })
+})
+router.delete("/:id",(req,res)=>{
+    const id=req.params.id
+    Employee.findByIdAndDelete(id).exec()
+    .then(employee=>{
+        if (!employee) {
+            return res.status(404).json({
+                message:"Employee with id specified not found!"
+            })
+        }
+        return res.status(200).json({
+            message:"Employee is removed!"
+        })
+    })
+    .catch(err=>{
+        return res.status(500).json({
+            message:"Internal error!",
+            error:err
+        })
     })
 })
 module.exports = router
